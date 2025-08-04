6a81e3a795b13d1424c6807ccbe12d42c646e585
Tapur83 <tapur4828.be23@chitkara.edu.in>
2025-08-05 01:20:00 +0530

    personalityQuiz

---
+++ labPersonalityQuiz/labPersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  labPersonalityQuiz
+//
+//  Created by Tapur Aggarwal on 23/07/25.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+}
+
